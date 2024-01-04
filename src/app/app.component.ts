eb53a137da8fdcd50b017d45ca72466d47d5958d
Jeremy Proot <jeremy.proot@outlook.com>
2024-01-04 10:46:10 -0500

    added translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TranslateModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'marriage-anouncement';
+  isChecked = false;
+
+  constructor(private translate: TranslateService) {}
+
+  onValueChange(): void {
+    if (!this.isChecked) {
+      this.translate.use('fr');
+    } else {
+      this.translate.use('nl');
+    }
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,32 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { TranslateLoader, TranslateModule, TranslateModuleConfig } from '@ngx-translate/core';
 
 import { routes } from './app.routes';
 
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, "./assets/i18n/", ".json");
+}
+
+const translationConfig : TranslateModuleConfig = {
+  defaultLanguage: 'fr',
+  loader: {
+    provide: TranslateLoader,
+    useFactory: HttpLoaderFactory,
+    deps: [HttpClient]
+}
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    importProvidersFrom([
+      TranslateModule.forRoot(translationConfig)
+    ]),
+  ]
 };
